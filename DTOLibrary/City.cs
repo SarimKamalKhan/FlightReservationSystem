7e0e178eb9c0e62be6b97c982c7cd9a44c929f60
Rabia Qaiser <Rabia.Qaiser@tpsonline.com>
2019-05-27 09:51:50 +0500

    remove extra classes form DTO

---
+++ DTOLibrary/City.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTOLibrary
+{
+    abstract class City
+    {
+        //schema's property here
+        public int  Id { get; set; }
+        public string Name { get; set; }
+        public string CountryCode { get; set; }
+
+        public string Status { get; set; }
+
+        public City()
+        {
+
+        }
+
+    }
+}
