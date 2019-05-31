f31b66f3507598fe31a2fe7bbbcf2559bc39f8b9
Rabia Qaiser <Rabia.Qaiser@tpsonline.com>
2019-05-31 12:32:44 +0500

    implement db calling for resrve flight

--- DataAccess/DatabaseFacade/DBManager.cs
+++ DataAccess/DatabaseFacade/DBManager.cs
@@ -1,4 +1,5 @@
 ﻿using DataAccess.DataBaseComponent;
+using DataTransferObjects;
 using DataTransferObjects.TravelCategories;
 using System.Data;
 
@@ -136,6 +137,53 @@ namespace DataAccess.DatabaseFacade
             }
 
             return ds_Responses;
+        }
+
+        public int ReserveFlight(FlightReservationRequestDTO flightReservationRequestDTO, out string spResponse)
+        {
+            string functionName = ".ReserveFlight";
+            string source = ClassName + functionName;
+            spResponse = string.Empty;
+            DataSet ds_Responses = new DataSet();
+            int rowsAffected = -1;
+            try
+            {
+
+                //1.Insert customer Reserve Flight details
+                //2.If successfully  then update available seats on reserve flight table and commit
+                //3.otherwise rollback
+
+
+                string spName = "PKG_FLIGHT_RESERVATION.ReserveFlight";
+
+                GeneralParams[] Params = new GeneralParams[8];
+
+                Params[0] = new GeneralParams("inReservationCode", 100, GeneralParams.GeneralDBTypes.VarChar, flightReservationRequestDTO.ReservationCode, ParameterDirection.Input);
+
+                Params[1] = new GeneralParams("inReservationNumber", 100, GeneralParams.GeneralDBTypes.VarChar, flightReservationRequestDTO.ReservationNumber, ParameterDirection.Input);
+
+                Params[2] = new GeneralParams("inCNIC", 100, GeneralParams.GeneralDBTypes.VarChar, flightReservationRequestDTO.CNIC, ParameterDirection.Input);
+
+                Params[3] = new GeneralParams("inMobileNumber", 100, GeneralParams.GeneralDBTypes.VarChar,flightReservationRequestDTO.MobileNumber, ParameterDirection.Input);
+
+                Params[4] = new GeneralParams("inReservedSeats", 100, GeneralParams.GeneralDBTypes.Int, flightReservationRequestDTO.ReservedSeats, ParameterDirection.Input);
+
+                Params[5] = new GeneralParams("inTotalAmount", 100, GeneralParams.GeneralDBTypes.Decimal, flightReservationRequestDTO.ReservedSeats, ParameterDirection.Input);
+
+                Params[6] = new GeneralParams("inUpdatedAvailableSeatsForReservation", 100, GeneralParams.GeneralDBTypes.Int, flightReservationRequestDTO.ReservedSeats, ParameterDirection.Input);
+
+                
+                Params[7] = new GeneralParams("outResponseCode", 3, GeneralParams.GeneralDBTypes.VarChar, null, ParameterDirection.Output);
+
+                rowsAffected = DBComponent.IDBMgr().ExecuteNonQuery(spName, Params);
+                spResponse = Params[7].OutputValue;
+            }
+            catch (System.Exception ex)
+            {
+                spResponse = CommonHelper.Constants.ResponseCodes.Failed;
+            }
+
+            return rowsAffected;
         }
     }
 }
--- DataAccess/Repositories/FlightReservation/ReserveFlightRepository.cs
+++ DataAccess/Repositories/FlightReservation/ReserveFlightRepository.cs
@@ -1,13 +1,37 @@
 ﻿using System;
+using CommonHelper.Constants;
+using DataAccess.DatabaseFacade;
 using DataTransferObjects;
 
 namespace DataAccess.Repositories.FlightReservation
 {
     public class ReserveFlightRepository : IReserveFlightRepository
     {
+        private string ClassName = "ReserveFlightRepository";
         public FlightReservationResponseDTO ReserveFlight(FlightReservationRequestDTO requestDTO, out string response)
         {
-            throw new NotImplementedException();
+            string functionName = ".ReserveFlight";
+            string source = ClassName + functionName;
+
+            response = string.Empty;
+
+            FlightReservationResponseDTO flightReservationResponseDTO = new FlightReservationResponseDTO();
+            DBManager dbMgr = new DBManager();
+
+            //1.Insert customer Reserve Flight details
+            //2.If successfully  then update available seats on reserve flight table and commit
+            //3.otherwise rollback
+
+            int rowsEffected = dbMgr.ReserveFlight(requestDTO, out response);
+
+            if (response == ResponseCodes.Success)
+            {
+                flightReservationResponseDTO.ReservationNumber = requestDTO.ReservationNumber;
+                flightReservationResponseDTO.ReserverdSeats = requestDTO.ReservedSeats;
+                flightReservationResponseDTO.TotalAmount = requestDTO.TotalAmount;
+            }
+
+            return flightReservationResponseDTO;
         }
     }
 }
